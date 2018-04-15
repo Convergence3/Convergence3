c265ea32f6833ec77369081cd60d6f79b7535817
Skillcheese <34143022+Skillcheese@users.noreply.github.com>
2018-04-15 11:28:36 -0500

    menu in cpp

---
+++ Source/Convergence3/Menu/MainMenu.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+
+UCLASS()
+class CONVERGENCE3_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual bool Initialize() override;
+
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Host;
+	
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Join;
+
+	UFUNCTION()
+	void HostServer();
+};
